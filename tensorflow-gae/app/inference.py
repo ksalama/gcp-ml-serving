41489f9f017d83d9158632ef0b55fcc80112ca53
khalid-m-salama <khalid.m.salama@gmail.com>
2018-03-08 00:47:01 +0000

    Fix inference in gae

--- tensorflow-gae/app/inference.py
+++ tensorflow-gae/app/inference.py
@@ -3,14 +3,28 @@ import tensorflow as tf
 SAVED_MODEL_DIR = 'models/babyweight-estimator-v1'
 
 
+predictor_fn = None
+
+
+def init_predictor():
+
+    global predictor_fn
+
+    if predictor_fn is None:
+
+        predictor_fn = tf.contrib.predictor.from_saved_model(
+            export_dir=SAVED_MODEL_DIR,
+            signature_def_key="predict"
+        )
+
+    return predictor_fn
+
+
 def estimate(instance):
 
     instance = dict((k, [v]) for k, v in instance.items())
 
-    predictor_fn = tf.contrib.predictor.from_saved_model(
-        export_dir= SAVED_MODEL_DIR,
-        signature_def_key="predict"
-    )
+    predictor_fn = init_predictor()
 
     value = predictor_fn(instance)['predictions'][0][0]
     output = {"estimate": str(value)}
